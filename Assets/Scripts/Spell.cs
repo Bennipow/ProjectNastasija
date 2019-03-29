f1be25552e7a25802fe98f852721bf566562c814
Bennipow <bg123@gmx.de>
2019-03-29 16:14:15 +0100

    Creating new Scripts

--- Assets/Scripts/Spell.cs
+++ Assets/Scripts/Spell.cs
@@ -22,14 +22,43 @@ public class Spell
     public readonly ECastType m_CastType;
     public readonly int m_Size;
 
+    public float m_SpellDamage;
+    public float m_ManaCost;    
+
     public Spell(int _size, ESpellType _spelltype, ECastType _casttype)
     {
         m_SpellType = _spelltype;
         m_CastType = _casttype;
+
     }
 
     public void CastSpell()
     {
+        CreateSpellStats();
         Debug.Log(m_SpellType + "" + m_CastType);
     }
+
+    /// <summary>
+    /// Create the Spell Stats from its element size and spell type
+    /// </summary>
+    private void CreateSpellStats()
+    {
+        switch (m_CastType)
+        {
+            case ECastType.Ball:
+                m_SpellDamage = m_Size * 10 + 30;
+                m_ManaCost = m_Size * 10 + 10;
+                break;
+            case ECastType.Lance:
+                m_SpellDamage = m_Size * 10 + 50; 
+                m_ManaCost = m_Size * 10 + 20;
+                break;
+            case ECastType.CircleGround:
+                m_SpellDamage = 50 - m_Size * 2; 
+                m_ManaCost = m_Size * 10 + 10;
+                break;
+            default:
+                break;
+        }
+    }    
 }
---
+++ Assets/Scripts/SpellObject.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpellObject : MonoBehaviour
+{
+    // Collider for Spell hit detection
+    Collider m_MyCollider;
+    // Particle System for Type of Spell
+    ParticleSystem m_MyParticleSystem;
+    // Particles for spell element
+    Sprite m_ParticleSprite;
+    // Spell with stats
+    Spell m_MySpell;
+}
